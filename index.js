11acc9839fb71fd66072f198e8d574fec7268598
HAKANKOKCU <103432992+HAKANKOKCU@users.noreply.github.com>
2022-04-11 18:27:19 +0300

    Added features
    Added Line width And Color

--- index.js
+++ index.js
@@ -2,45 +2,50 @@ var isdrawing = false;
 var draw = document.getElementById("draw");
 var ctx = draw.getContext("2d");
 var oldx,oldy;
-draw.width = window.innerWidth * 4;
-draw.height = window.innerHeight * 4;
+draw.width = window.innerWidth * 3;
+draw.height = window.innerHeight * 3;
 draw.style.width = window.innerWidth + "px";
 draw.style.height = window.innerHeight + "px";
 ctx.fillStyle = "white";
 ctx.fillRect(0, 0, draw.width, draw.height);
-ctx.fillStyle = "black";
+ctx.fillStyle = document.getElementById("dcolor").value;
+ctx.lineWidth = 6;
 function down() {isdrawing = true}
 function up() {isdrawing = false}
 function move(e) {
   if (isdrawing) {
+    ctx.fillStyle = document.getElementById("dcolor").value;
+    ctx.strokeStyle = document.getElementById("dcolor").value;
+    console.log(document.getElementById("dcolor").value)
     ctx.beginPath();
-	ctx.lineWidth = 6;
-    ctx.arc(e.offsetX * 4,e.offsetY * 4,3,0,2*Math.PI);
+    ctx.arc(e.offsetX * 3,e.offsetY * 3,ctx.lineWidth / 2,0,2*Math.PI,false);
     ctx.fill();
-	ctx.beginPath();
-	ctx.moveTo(oldx, oldy);
-	ctx.lineTo(e.offsetX * 4, e.offsetY * 4);
-	ctx.stroke(); 
+    ctx.beginPath();
+    ctx.arc(oldx,oldy ,ctx.lineWidth / 2,0,2*Math.PI,false);
+    ctx.fill();
+	  ctx.beginPath();
+	  ctx.moveTo(oldx, oldy);
+	  ctx.lineTo(e.offsetX * 3, e.offsetY * 3);
+	  ctx.stroke();
   }
-  oldx = e.offsetX * 4;
-  oldy = e.offsetY * 4
+  oldx = e.offsetX * 3;
+  oldy = e.offsetY * 3
 }
 function setSize() {
-	ctx.clearRect(0, 0, draw.width, draw.height);
+  draw.width = window.innerWidth * 3;
+	draw.height = window.innerHeight * 3;
+	draw.style.width = window.innerWidth + "px";
+	draw.style.height = window.innerHeight + "px";
 	ctx.fillStyle = "white";
 	ctx.fillRect(0, 0, draw.width, draw.height);
-	ctx.fillStyle = "black";
-	draw.width = window.innerWidth * 4;
-	draw.height = window.innerHeight * 4;
-	draw.style.width = window.innerWidth;
-	draw.style.height = window.innerHeight;
+	ctx.fillStyle = document.getElementById("dcolor").value;
 }
 function save() {
 	let canvasUrl = draw.toDataURL("image/jpeg", 0.5);
 	console.log(canvasUrl);
 	const createEl = document.createElement('a');
 	createEl.href = canvasUrl;
-	createEl.download = "download-this-canvas";
+	createEl.download = "download";
 	createEl.click();
 	createEl.remove();
 	var x = document.getElementById("snackbar");
@@ -52,4 +57,23 @@ function clearAll() {
 	ctx.fillStyle = "white";
 	ctx.fillRect(0, 0, draw.width, draw.height);
 	ctx.fillStyle = "black";
+}
+function toglevisi(elementID) {
+  var element = document.getElementById(elementID)
+  if (element.style.display === "block") {
+    element.style.display = "none"
+  }else {
+    element.style.display = "block"
+  }
+}
+for (let i = 1; i < 201; i++) {
+  var btn = document.createElement("button")
+  btn.innerText = i;
+  btn.style.borderRadius = "5px";
+  btn.style.width = "100%";
+  btn.onclick = function () {
+    toglevisi("sz");
+    ctx.lineWidth = i * 3;
+  };
+  document.getElementById("sz").appendChild(btn)
 }
