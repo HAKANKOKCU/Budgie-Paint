40cda5432765cf1069bf7324f10425c4f24eadd2
HAKANKOKCU <103432992+HAKANKOKCU@users.noreply.github.com>
2022-04-12 10:34:20 +0300

    Changed Window Size

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const { app, BrowserWindow } = require('electron')
 const path = require('path')
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 700,
 	frame: true,
 	nodeIntegration: true,
         enableRemoteModule: true,
